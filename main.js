2b83296b0a447f6643150e619bc88dc824c0749c
Billy Osaye <osayebilly98@gmail.com>
2024-06-29 07:42:56 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,5 @@
+// main.js
+import { add, subtract } from './mathFunctions.js';
+
+console.log(add(5, 3)); 
+console.log(subtract(10, 4)); 
---
+++ mathFunctions.js
@@ -0,0 +1,9 @@
+// mathFunctions.js
+export function add(a, b) {
+    return a + b;
+  }
+  
+  export function subtract(a, b) {
+    return a - b;
+  }
+  
